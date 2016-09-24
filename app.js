aad3fd1f8b65b688799332cc5f87d779df68c6fd
yashwanth <yashwanthreddy.g@live.com>
2016-09-24 22:31:16 +0530

    cleaned up boiler plate express code

--- app.js
+++ app.js
@@ -5,22 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 var eventDict = {
     'my.sample.package.SAMPLE_EVENT1':{
@@ -36,6 +25,10 @@ var eventDict = {
         listeners:["localhost:2121","localhost:3121"]
     }
 }
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
 app.get('/emitters/:evid', function (req, res) {
     if(eventDict[req.params.evid])
@@ -70,29 +63,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-
-
-module.exports = app;
